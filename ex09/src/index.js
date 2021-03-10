7ef4f632eed01cf17c2a95886e80582517667d81
AdnanD5 <adodervisevic55@hotmail.com>
2021-03-10 21:48:02 +0100

    Dodavanje ex09

---
+++ ex09/src/index.js
@@ -0,0 +1,10 @@
+function main(numOne, numTwo){
+    //Below
+
+var difference = numOne - numTwo;
+    //Above
+    return difference;
+}
+
+console.log(main(numOne, numTwo));
+module.exports = main;
