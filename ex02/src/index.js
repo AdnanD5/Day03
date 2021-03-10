45e5319cd4c4730236ff53f697aae409514b1174
AdnanD5 <adodervisevic55@hotmail.com>
2021-03-10 19:31:36 +0100

    Dodavanje ex02

---
+++ ex02/src/index.js
@@ -0,0 +1,16 @@
+function main(){
+    // Only change code below this line
+
+    // Only change code above this line
+
+    //Komentari su dozvoljeni u ovom redu
+
+    /* Komentari su dozvoljeni
+    Skoro svuda
+    
+    Čak i ovdje*/
+}
+
+console.log(main());
+
+module.exports = main;
