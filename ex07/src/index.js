1e608a8486e17d4a585df7f8a10dce137e5d20d7
AdnanD5 <adodervisevic55@hotmail.com>
2021-03-10 20:16:25 +0100

    Dodavanje ex07

---
+++ ex07/src/index.js
@@ -0,0 +1,19 @@
+function main(){
+    //below
+    //declarations
+var studlyCapVar
+var properCamelCase
+var titleCaseOver
+    //assignments
+    studlyCapVar = 10;
+    properCamelCase = "A String";
+    titleCaseOver = 9000;
+    //above
+return{
+    studlyCapVar,
+    properCamelCase,
+    titleCaseOver
+}
+}
+console.log(main());
+module.exports = main;
